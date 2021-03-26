792cbe3e4c0836ddd507805a8e766deafb768f4a
Aidan Harbison <aharbis@us.ibm.com>
2021-03-26 13:50:23 +0000

    Content promotion

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -6,7 +6,7 @@ import { Link } from 'gatsby'
 
 import Carbon from '../../images/carbon.jpg';
 
-const FirstLeftText = () => <p>Version 1.2.2</p>;
+const FirstLeftText = () => <p>Version 1.3.0</p>;
 
 const FirstRightText = () => (
   <p>
