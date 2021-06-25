c2fd50905532c4fdf9533d2cc3454027d6e570f6
Aidan Harbison <aharbis@us.ibm.com>
2021-06-25 16:35:58 +0000

    Content promotion

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -6,7 +6,7 @@ import { Link } from 'gatsby'
 
 import Carbon from '../../images/carbon.jpg';
 
-const FirstLeftText = () => <p>Version 1.3.1</p>;
+const FirstLeftText = () => <p>Version 1.4.0</p>;
 
 const FirstRightText = () => (
   <p>
@@ -14,7 +14,7 @@ const FirstRightText = () => (
 
     <div className={calloutLink}>
       <Link
-        to="release-notes/cd#1.3.1"
+        to="release-notes/cd#1.4.0"
       >
         Release notes →
       </Link>
